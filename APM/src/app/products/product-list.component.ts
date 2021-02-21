0b2b4fd56b3a6d9302c957bc2b4209aee0920de1
Sara Decker <sdecker@west-inc.com>
2021-02-21 15:16:12 -0700

    use getter/setter for two-way-binding of listFilter; use @ViewChild decorator to reference the filter element and set focus to it

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,46 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 
-import { IProduct } from './product';
-import { ProductService } from './product.service';
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List';
-    listFilter: string;
-    showImage: boolean;
+export class ProductListComponent implements OnInit, AfterViewInit {
+  pageTitle: string = "Product List";
+  showImage: boolean;
 
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    errorMessage: string;
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  errorMessage: string;
 
-    filteredProducts: IProduct[];
-    products: IProduct[];
+  filteredProducts: IProduct[];
+  products: IProduct[];
 
-    constructor(private productService: ProductService) { }
+  @ViewChild("filterElement") filterElementRef: ElementRef;
 
-    ngOnInit(): void {
-        this.productService.getProducts().subscribe(
-            (products: IProduct[]) => {
-                this.products = products;
-                this.performFilter(this.listFilter);
-            },
-            (error: any) => this.errorMessage = <any>error
-        );
+  private _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.performFilter(value);
+  }
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      (products: IProduct[]) => {
+        this.products = products;
+        this.performFilter(this.listFilter);
+      },
+      (error: any) => (this.errorMessage = <any>error)
+    );
+  }
+
+  ngAfterViewInit(): void {
+    // filterElementRef would not have been available in the
+    // ngOnInit() hook.
+    this.filterElementRef.nativeElement.focus();
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy?: string): void {
+    if (filterBy) {
+      this.filteredProducts = this.products.filter(
+        (product: IProduct) =>
+          product.productName
+            .toLocaleLowerCase()
+            .indexOf(filterBy.toLocaleLowerCase()) !== -1
+      );
+    } else {
+      this.filteredProducts = this.products;
     }
-
-    toggleImage(): void {
-        this.showImage = !this.showImage;
-    }
-
-    performFilter(filterBy?: string): void {
-        if (filterBy) {
-            this.filteredProducts = this.products.filter((product: IProduct) =>
-                product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1);
-        } else {
-            this.filteredProducts = this.products;
-        }
-    }
+  }
 }

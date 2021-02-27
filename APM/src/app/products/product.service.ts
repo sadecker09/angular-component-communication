388b3a77d5bf09ea992506fc083e9166a3450638
Sara Decker <sdecker@west-inc.com>
2021-02-27 15:09:05 -0700

    add functionality to the state management service to create and delete products - store the data in memory so doesn't need to be retrieved on each request

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -12,13 +12,18 @@ import { IProduct } from './product';
 @Injectable()
 export class ProductService {
     private productsUrl = 'api/products';
+    private products: IProduct[];
 
     constructor(private http: HttpClient) { }
 
-    getProducts(): Observable<IProduct[]> {
+    getProducts(): Observable<IProduct[]> {\
+        if(this.products) {
+            return of(this.products);
+        }
         return this.http.get<IProduct[]>(this.productsUrl)
                         .pipe(
                             tap(data => console.log(JSON.stringify(data))),
+                            tap(data => this.products = data),
                             catchError(this.handleError)
                         );
     }
@@ -26,6 +31,12 @@ export class ProductService {
     getProduct(id: number): Observable<IProduct> {
         if (id === 0) {
             return of(this.initializeProduct());
+        }
+        if(this.products) {
+            const foundItem = this.products.find(item => item.id === id);
+            if(foundItem) {
+                return of(foundItem);
+            }
         }
         const url = `${this.productsUrl}/${id}`;
         return this.http.get<IProduct>(url)
@@ -50,6 +61,12 @@ export class ProductService {
         return this.http.delete<IProduct>(url, { headers: headers} )
                         .pipe(
                             tap(data => console.log('deleteProduct: ' + id)),
+                            tap(data => {
+                                const foundIndex = this.products.findIndex(item => item.id === id);
+                                if(foundIndex > -1) {
+                                    this.products.splice(foundIndex, 1);
+                                }
+                            }),
                             catchError(this.handleError)
                         );
     }
@@ -59,6 +76,7 @@ export class ProductService {
         return this.http.post<IProduct>(this.productsUrl, product,  { headers: headers} )
                         .pipe(
                             tap(data => console.log('createProduct: ' + JSON.stringify(data))),
+                            tap(data => this.products.push(data)),
                             catchError(this.handleError)
                         );
     }

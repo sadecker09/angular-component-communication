374979276e371f85d94813940dd6012039a533f7
Sara Decker <sdecker@west-inc.com>
2021-02-27 15:39:55 -0700

    change up the ui a bit so that there are two components side by side - one to show a list of the products which are clickable buttons and on select shows the product details in a detail component on the right; keep the selected product in state and sync this across components

--- APM/src/app/products/product-shell/product-shell-detail.component.ts
+++ APM/src/app/products/product-shell/product-shell-detail.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
-import { IProduct } from '../product';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { IProduct } from "../product";
 
 @Component({
-    selector: 'pm-product-shell-detail',
-    templateUrl: './product-shell-detail.component.html'
+  selector: "pm-product-shell-detail",
+  templateUrl: "./product-shell-detail.component.html",
 })
 export class ProductShellDetailComponent implements OnInit {
-    pageTitle: string = 'Product Detail';
+  pageTitle: string = "Product Detail";
 
-    constructor(private productService: ProductService) { }
+  // this getter will watch for changes in the service
+  // and template will update accordingly
+  get product(): IProduct | null {
+    return this.productService.currentProduct;
+  }
+  constructor(private productService: ProductService) {}
 
-    ngOnInit() {
-    }
-
+  ngOnInit() {}
 }
